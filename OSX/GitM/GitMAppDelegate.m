5b75bc444ca03052ac395d08f5609604338b8ebc
datalurkur <datalurkur@gmail.com>
2011-09-22 15:37:48 -0500

    Rework of project targets in OSX

---
+++ OSX/GitM/GitMAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  GitMAppDelegate.m
+//  GitM
+//
+//  Created by Andrew Jean on 9/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "GitMAppDelegate.h"
+
+@implementation GitMAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end

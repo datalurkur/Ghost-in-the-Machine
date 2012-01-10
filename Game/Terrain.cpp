afba8442fc51a55b8830171d6235388e3c4e524d
Andrew Jean <datalurkur@gmail.com>
2012-01-09 20:33:39 -0600

    Gets simple randomly generated terrain rendering.

--- Game/Terrain.cpp
+++ Game/Terrain.cpp
@@ -1,11 +1,14 @@
 #include <Game/Terrain.h>
 #include <Resource/MaterialManager.h>
 
+#define TILE(i,j) _tileData[(j * (int)dims.x) + i]
+#define FLAGS(i,j) _faceFlags[(j * (int)dims.x) + i]
+
 const std::string Terrain::NodeType = "Terrain";
 
 Terrain::Terrain(const Vector2 &dims): SceneNode("Terrain", NodeType) {
 	setDimensions(dims);
-	_tileData = (TerrainType*)calloc((size_t)(getDimensions().arraySize()), sizeof(TerrainType));
+	generateMap();
 	populateScene();
 }
 
@@ -13,23 +16,151 @@ Terrain::~Terrain() {
 	free(_tileData);
 }
 
-void Terrain::populateScene() {
+/*
+Terrain::TerrainType Terrain::getType(int x, int y) const {
+	return (TerrainType)_tileData[(x * (int)getDimensions().y) + y];
+}
+
+Terrain::FaceFlags Terrain::getFlags(int x, int y) const {
+	return (FaceFlags)_faceFlags[(x * (int)getDimensions().y) + y];
+}
+*/
+
+void Terrain::generateMap() {
 	Vector2 dims = getDimensions();
 
-	Renderable *tRend = new Renderable();
-	
-	float verts[12] = {
-		0,      0,      0,
-		dims.x, 0,      0,
-		dims.x, dims.y, 0,
-		0,      dims.y, 0
-	};
-	unsigned int indices[4] = { 0, 1, 2, 3 };
+	_tileData = (TerrainType*)calloc((size_t)(dims.arraySize()), sizeof(TerrainType));
+	_faceFlags = (unsigned char*)calloc((size_t)(dims.arraySize()), sizeof(unsigned char));
 
-	tRend->setVertexPointer(&verts[0], 4, 3);
-	tRend->setIndexPointer(&indices[0], 4);
-	tRend->setMaterial(MaterialManager::Get("debug"));
+	// For now, just seed the map randomly
+	for(int i=0; i < dims.x; i++) {
+		for(int j=0; j < dims.y; j++) {
+			TILE(i,j) = (TerrainType)(rand() % NUM_TILE_TYPES);
+			//TILE(i,j) = EMPTY;
+		}
+	}
+
+	regenFaceFlags();
+}
+
+void Terrain::regenFaceFlags() {
+	int i, j;
+	Vector2 dims;
+
+	dims = getDimensions();
+
+	for(i=0; i<dims.x; i++) {
+		for(j=0; j<dims.y; j++) {
+			FLAGS(i,j) = 0;
+			if(TILE(i,j) == EMPTY) {
+				FLAGS(i,j) |= BOTTOM;
+			} else {
+				FLAGS(i,j) |= TOP;
+				if(i == 0          || TILE(i-1, j) == EMPTY) { FLAGS(i,j) |= WEST;  }
+				if(j == 0          || TILE(i, j-1) == EMPTY) { FLAGS(i,j) |= SOUTH; }
+				if(i == (dims.x-1) || TILE(i+1, j) == EMPTY) { FLAGS(i,j) |= EAST;  }
+				if(j == (dims.y-1) || TILE(i, j+1) == EMPTY) { FLAGS(i,j) |= NORTH; }
+			}
+		}
+	}
+}
+
+void Terrain::populateScene() {
+	Vector2 dims;
+	Renderable *topRenderable, *bottomRenderable;
+	int vertexSize, layers, vertsPerLayer, totalVerts,
+		i, j;
+	float *verts;
+	std::vector<unsigned int> bottomIndices;
+	std::vector<unsigned int> topIndices;
+
+	dims = getDimensions();
+
+	layers		  = 2;
+	vertexSize	  = 3;
+	vertsPerLayer = ((int)dims.x + 1) * ((int)dims.y + 1);
+	totalVerts    = vertsPerLayer * layers;
+
+	// Allocate memory for all the vertices
+	verts = (float*)calloc(totalVerts * vertexSize, sizeof(float));
+
+#define INDEX(i,j,k) (((k) * vertsPerLayer) + ((j) * ((int)dims.x+1)) + (i))
+
+	// Define all the vertices, both upper and lower
+	// TODO - lots of redundant verts generated here, consider optimising
+	for(i=0; i<=dims.x; i++) {
+		for(j=0; j<=dims.y; j++) {
+			float *top, *bottom;
+			top    = &verts[(INDEX(i,j,1)*3)];
+			bottom = &verts[(INDEX(i,j,0)*3)];
+			top[0] = (float)i;
+			top[1] = (float)j;
+			top[2] = 1.0f;
+			bottom[0] = (float)i;
+			bottom[1] = (float)j;
+			bottom[2] = 0.0f;
+		}
+	}
+
+	for(i=0; i<dims.x; i++) {
+		for(j=0; j<dims.y; j++) {
+			unsigned char flags = FLAGS(i,j);
+			if(flags & BOTTOM) {
+				bottomIndices.push_back(INDEX(i,  j,  0));
+				bottomIndices.push_back(INDEX(i+1,j,  0));
+				bottomIndices.push_back(INDEX(i+1,j+1,0));
+				bottomIndices.push_back(INDEX(i,  j+1,0));
+			} else {
+				topIndices.push_back(INDEX(i,  j,  1));
+				topIndices.push_back(INDEX(i+1,j,  1));
+				topIndices.push_back(INDEX(i+1,j+1,1));
+				topIndices.push_back(INDEX(i,  j+1,1));
+				
+				if(flags & EAST) {
+					topIndices.push_back(INDEX(i+1,j,  1));
+					topIndices.push_back(INDEX(i+1,j,  0));
+					topIndices.push_back(INDEX(i+1,j+1,0));
+					topIndices.push_back(INDEX(i+1,j+1,1));
+				}
+				if(flags & WEST) {
+					topIndices.push_back(INDEX(i,j,  0));
+					topIndices.push_back(INDEX(i,j,  1));
+					topIndices.push_back(INDEX(i,j+1,1));
+					topIndices.push_back(INDEX(i,j+1,0));
+				}
+				if(flags & SOUTH) {
+					topIndices.push_back(INDEX(i  ,j,0));
+					topIndices.push_back(INDEX(i  ,j,1));
+					topIndices.push_back(INDEX(i+1,j,1));
+					topIndices.push_back(INDEX(i+1,j,0));
+				}
+				if(flags & NORTH) {
+					topIndices.push_back(INDEX(i  ,j+1,1));
+					topIndices.push_back(INDEX(i  ,j+1,0));
+					topIndices.push_back(INDEX(i+1,j+1,0));
+					topIndices.push_back(INDEX(i+1,j+1,1));
+				}
+			}
+		}
+	}
 
 	clearRenderables();
-	addRenderable(tRend);
+
+	bottomRenderable = new Renderable();
+	bottomRenderable->setVertexPointer(&verts[0], totalVerts, 3);
+	bottomRenderable->setIndexPointer(&bottomIndices[0], bottomIndices.size());
+	bottomRenderable->setMaterial(MaterialManager::Get("grass"));
+	addRenderable(bottomRenderable);
+	topRenderable = new Renderable();
+	topRenderable->setVertexPointer(&verts[0], totalVerts, 3);
+	topRenderable->setIndexPointer(&topIndices[0], topIndices.size());
+	topRenderable->setMaterial(MaterialManager::Get("wall"));
+	addRenderable(topRenderable);
+
+	free(verts);
+
+#undef INDEX
 }
+
+#undef TILE
+#undef FLAGS
--- Game/Terrain.h
+++ Game/Terrain.h
@@ -9,21 +9,38 @@ public:
 	static const std::string NodeType;
 
 public:
-	Terrain(const Vector2 &dims);
-	~Terrain();
-
-public:
 	enum TerrainType {
 		EMPTY = 0,
 		WALL,
-		IMPENETRABLE
+		IMPENETRABLE,
+		NUM_TILE_TYPES
+	};
+	enum FaceFlags {
+		TOP    = 0x01,
+		BOTTOM = 0x02,
+		NORTH  = 0x04,
+		SOUTH  = 0x08,
+		EAST   = 0x10,
+		WEST   = 0x20
 	};
 
+public:
+	Terrain(const Vector2 &dims);
+	~Terrain();
+
+	/*
+	TerrainType getType(int x, int y) const;
+	FaceFlags getFlags(int x, int y) const;
+	*/
+
 private:
+	void generateMap();
+	void regenFaceFlags();
 	void populateScene();
 
 private:
 	TerrainType *_tileData;
+	unsigned char *_faceFlags;
 };
 
 #endif

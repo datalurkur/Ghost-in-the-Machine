f8ac3025626688fd67e049ea179f8cb1494c9af1
Andrew Jean <datalurkur@gmail.com>
2012-02-25 16:56:56 -0600

    Temporary IsoCamera code.

--- Engine/IsoCamera.cpp
+++ Engine/IsoCamera.cpp
@@ -1,6 +1,7 @@
 #include <Engine/IsoCamera.h>
 
 IsoCamera::IsoCamera(const std::string &name): Camera(name), _fov(STANDARD_FOV), _near(10.0f), _far(-10.0f) {
+	setModelView(Matrix4::MakeTranslation(-2.0f, -2.0f, -3.0f));
 }
 
 IsoCamera::~IsoCamera() {
@@ -22,9 +23,11 @@ void IsoCamera::moveRelative(const Vector3 &dir) {
 }
 
 void IsoCamera::zoom(const float amount) {
+	moveRelative(Vector3(0.0f, 0.0f, amount));
 }
 
 void IsoCamera::scroll(const Vector3 &dir) {
+	moveRelative(dir);
 }
 
 void IsoCamera::pitch(const float amount) {

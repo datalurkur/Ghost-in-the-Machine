7050605a61a1c0ca5bc9ba1b21b4aa330beafaa9
Andrew Jean <datalurkur@gmail.com>
2012-03-29 22:58:12 -0500

    Updates the Win7 project

--- Game/PlayingState.cpp
+++ Game/PlayingState.cpp
@@ -38,7 +38,7 @@ void PlayingState::setup(va_list args) {
 
 	// DEBUG
 	// Add some test UIElements
-	_ui->addNode(UIElement::Box(Vector2(-0.5f, -0.5f), Vector2(1.0f, 1.0f), Color4(1.0f, 1.0f, 1.0f, 1.0f)));
+	_ui->addNode(UIElement::Text(Vector2(-0.5f, -0.5f), "TEST Text!", "acknowledge.ttf"));
 
 	// Update the world and UIManager once with no time to make sure the scene gets populated fully before rendering
 	_world->update(0);
